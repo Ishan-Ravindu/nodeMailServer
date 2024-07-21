9e9a7271a4b664385f6f416e7e39f43702279121
Ishan-Ravindu <ishanravindu27596@gmail.com>
2024-07-21 12:47:15 +0530

    fix(env): move secure content to .evn file

--- src/util/send_mail.ts
+++ src/util/send_mail.ts
@@ -1,18 +1,21 @@
-import * as nodemailer from 'nodemailer';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
 
-const SMTP_HOST = '*********';
-const SMTP_PORT = 587;
-const SMTP_USER = '*********';
-const SMTP_PASS = '*********';
+dotenv.config();
+
+const SMTP_HOST = process.env.SMTP_HOST;
+const SMTP_PORT = parseInt(process.env.SMTP_PORT || '587');
+const SMTP_USER = process.env.SMTP_USER;
+const SMTP_PASS = process.env.SMTP_PASS;
 
 const transporter = nodemailer.createTransport({
   host: SMTP_HOST,
   port: SMTP_PORT,
-  secure: false, // Use TLS
+  secure: false,
   auth: {
     user: SMTP_USER,
     pass: SMTP_PASS,
   },
-});
+} as nodemailer.TransportOptions);
 
 export default transporter;
